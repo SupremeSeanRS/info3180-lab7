39605d366ae41901b19c1eb4648a63a53998ff7a
EC2 Default User <ec2-user@ip-172-31-92-176.ec2.internal>
2020-05-02 21:38:08 +0000

    complete ex1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired, DataRequired
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Message', validators=[DataRequired()])
+    photo = FileField('Photo', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    
